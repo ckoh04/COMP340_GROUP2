406cf96195c709a98860dcecf87bfcfda0026bcf
Chan Young Koh <c.young.koh@gmail.com>
2018-04-17 19:55:56 -0400

    No module name "django_rest_framework" error popping

--- gameserver/gameserver/settings.py
+++ gameserver/gameserver/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+   # 'module',
+    'django_rest_framework',
 ]
 
 MIDDLEWARE = [
--- gameserver/user/models.py
+++ gameserver/user/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=15)
+    id = models.AutoField(primary_key=True)
