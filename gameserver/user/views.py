51642cf2833db49e4682e78ac18e22afffbd2a22
Chan Young Koh <c.young.koh@gmail.com>
2018-04-27 18:55:13 -0400

    fixed views.py bug from unfinished code

--- gameserver/user/views.py
+++ gameserver/user/views.py
@@ -1,23 +1,33 @@
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
+from django.core.exceptions import ObjectDoesNotExist
 import json
+from user.models import User
+from django.views.decorators.csrf import csrf_exempt
 
 class HomePageView(APIView):
-	@csrf_exempt
-	
-	def create_or_retrieve(self, request = None, uname ="test", format=None):
 
-		if request.method == "POST":
-			try: 
-				found_user = user.objects.get(name=uname)
-				return HttpResponse(json.dumps({"status": "AlreadyExists"}), status = 403)
-			except ObjectDoesNotExist as e: 
-				pass 
+    @csrf_exempt
+    def create_or_retrieve(self, request=None, uname="test", format=None):
+        if request.method == "GET":
+            try:
+                found_user = User.objects.get(name=uname)
+                return HttpResponse(json.dumps({"status":"Success"}), status=200)
+            except ObjectDoesNotExist as e:
+                return HttpResponse(json.dumps({"status":"NoSuchUser"}), status=404)
 
-			u = User(name =uname)
-			u.save()
-			return HttpResponse(json.dumps("status":"Success"))
+        if request.method == "POST":
+            try:
+                found_user = User.objects.get(name=uname)
+                return HttpResponse(json.dumps({"status":"AlreadyExists"}), status=403)
+            except ObjectDoesNotExist as e:
+                pass
+            u = User(name=uname)
+            u.save()
+            return HttpResponse(json.dumps({"status":"Success"}))
+
+        data = { "user": uname, "id": found_user.id }
+        return HttpResponse(json.dumps(data))
 
 
     
