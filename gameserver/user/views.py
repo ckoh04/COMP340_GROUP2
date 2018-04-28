287c3abb96274fc848255bc63e72330562cc934e
Chan Young Koh <c.young.koh@gmail.com>
2018-04-27 20:57:12 -0400

    update on views.py

--- gameserver/user/views.py
+++ gameserver/user/views.py
@@ -1,18 +1,25 @@
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from django.core.exceptions import ObjectDoesNotExist
-import json
 from user.models import User
 from django.views.decorators.csrf import csrf_exempt
+
+import json
+
+
+
 
 class HomePageView(APIView):
 
     @csrf_exempt
     def create_or_retrieve(self, request=None, uname="test", format=None):
-        if request.method == "GET":
+        if request.method == 'GET':
+
             try:
                 found_user = User.objects.get(name=uname)
-                return HttpResponse(json.dumps({"status":"Success"}), status=200)
+                data = { "user": uname, "id": found_user.id }
+                return HttpResponse(json.dumps(data), status=200)
+
             except ObjectDoesNotExist as e:
                 return HttpResponse(json.dumps({"status":"NoSuchUser"}), status=404)
 
@@ -25,12 +32,3 @@ class HomePageView(APIView):
             u = User(name=uname)
             u.save()
             return HttpResponse(json.dumps({"status":"Success"}))
-
-        data = { "user": uname, "id": found_user.id }
-        return HttpResponse(json.dumps(data))
-
-
-    
-   # def get(self, request=None, uname="test", format=None):
-   #     data = { "user": uname, "key2": "value2" }
-   #     return HttpResponse(json.dumps(data))
